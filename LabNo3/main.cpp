623d794186512e35ef915b96f281da9dfde45c8c
PekaEboy <pekaeboy@gmail.com>
2023-09-24 22:50:22 +0300

    Добавьте файлы проекта.

---
+++ LabNo3/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <Windows.h>
+#include <locale>
+
+// Сумма (-1)^n * x^(2n+1) / (2n+1)!, ввод от пользователя: e, x
+// Это оказывается ряд Маклорена для sin(x)
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    
+    double x, e;
+    cout << "Введите число x в бесконечном сумме: ";
+    cin >> x;
+    cout << "Введите число e для точности: ";
+    cin >> e;
+
+    double delta = x;
+    double summar = 0;
+    int k = 1;
+    do {
+        summar += delta;
+        delta *= (( - 1.0 * pow(x, 2)) / (2 * k * (2 * k + 1)));
+        k++;
+    } while (abs(delta) > e);
+    cout << "Итоговая сумма (или sin(x)): " << summar << endl;
+    system("pause");
+    return 0;
+}
